d2d82a96efe89bdef825ab3602f77f765175dc71
phyoewaiaung <phyoewaiaung082@gmail.com>
2023-09-09 22:29:35 +0630

    watch chatroom page and redirect to welcome page if user logout

--- src/views/ChatRoom.vue
+++ src/views/ChatRoom.vue
@@ -5,9 +5,21 @@
 </template>
 
 <script>
+import { watch } from 'vue';
 import Navbar from "../components/navbar";
+import getUser from '@/composables/getUser';
+import { useRouter } from 'vue-router';
 export default {
   components: { Navbar },
+  setup(){
+    let {user} = getUser();
+    let router = useRouter();
+    watch(user,()=> {
+      if(!user.value){
+        router.push({name:"Welcome"})
+      }
+    })
+  }
 };
 </script>
 
