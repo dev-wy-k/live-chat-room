1682b3c37137c7c23b9d15fa4e99b7feeb0ce0db
phyoewaiaung <phyoewaiaung082@gmail.com>
2023-09-09 21:59:28 +0630

    add route guard to chatroom page

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import "./assets/styles.css"
-createApp(App).use(router).mount('#app')
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import "./assets/styles.css";
+import { auth } from "./firebase/config";
+
+let app;
+auth.onAuthStateChanged(() => {
+  if (!app) {
+    app = createApp(App).use(router).mount("#app");
+  }
+});
